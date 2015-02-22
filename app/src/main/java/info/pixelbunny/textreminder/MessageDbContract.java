8f49d9c32274077b3a7f4e8794ec476e8c4677da
Pixie <meggsage@gmail.com>
2015-02-21 23:41:53 -0800

    created db contract class

---
+++ app/src/main/java/info/pixelbunny/textreminder/MessageDbContract.java
@@ -0,0 +1,20 @@
+package info.pixelbunny.textreminder;
+import android.provider.*;
+
+public final class MessageDbContract {
+
+	public MessageDbContract() {
+		//empty constructor
+	}
+
+	public static abstract class MessageEntry implements BaseColumns {
+
+		public final static String TABLE_NAME = "messages";
+		public final static String COLUMN_MESSAGE_ID = "messageId";
+		public final static String COLUMN_MESSAGE_TEXT = "messageText";
+		public final static String COLUMN_MESSAGE_PHONENUMBER = "messagePhonenumber";
+		public final static String[] PROJECTION_ALL = { COLUMN_MESSAGE_ID, COLUMN_MESSAGE_TEXT, 
+			COLUMN_MESSAGE_PHONENUMBER };
+	}
+
+}
--- app/src/main/java/info/pixelbunny/textreminder/MessageDbHelper.java
+++ app/src/main/java/info/pixelbunny/textreminder/MessageDbHelper.java
@@ -3,6 +3,7 @@ package info.pixelbunny.textreminder;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.*;
 
 /**
  * Created by meggz on 14/02/15.
@@ -11,6 +12,10 @@ public class MessageDbHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Message.db";
+	
+	private static final String CREATE_TABLE_QUERY = "CREATE TABLE " + DATABASE_NAME
+		+ DATABASE_NAME + " (" + MessageDbContract.MessageEntry.TABLE_NAME +
+		;
 
     public MessageDbHelper (Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,7 +27,9 @@ public class MessageDbHelper extends SQLiteOpenHelper {
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-
     }
 
+	//public final static String COLUMN_MESSAGE_ID = "messageId";
 }
+
+
